08ff2168972f42917cb992fc8445e5eb8fb85f79
David <davidandrino2002@gmail.com>
2023-03-04 21:18:03 +0100

    New start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,61 @@
 #!/bin/bash
-apt update && apt upgrade -y
-apt install -y git neovim clangd wget openjdk-19-jdk code unzip
 
-# Install nodejs
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-apt-get install -y nodejs
+# Initial Declarations
+install_packages="build-essential wget zsh neovim gh tmux alacritty"
+hack_nerd_font_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip"
 
-# Install Plug
-if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
-	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-		   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-fi
-nvim +'PlugInstall' +qall
+# Enforce root
+if [ "$UID" -ne "$ROOT_UID" ]
+then
+  echo "Must be root to run this script."
+  exit $E_NOTROOT
+fi  
 
-# Install Git config 
-cd /tmp/
-git clone https://github.com/soraspades/startupconfig
-cd StartupConfig
-cp -r nvim/* ~/.config/nvim/
+# Install add-apt-repository, curl
+sudo apt update && sudo apt upgrade -y
+sudo apt install software-properties-common curl
+
+# Install gh cli
+type -p curl >/dev/null || sudo apt install curl -y
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
+# Add neovim, alacritty ppa
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo add-apt-repository ppa:aslatter/ppa -y
+
+# Update, upgrade and install packages
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y $install_packages
+
+# Clone repo into /tmp
+cd /tmp
+git clone https://github.com/soraspades/startupconfig /tmp/startupconfig
+
+# Install Hack NF
+wget $hack_nerd_font_url -P /tmp
+unzip /tmp/Hack.zip -d /tmp
+mkdir -p ~/.local/share/fonts/HackNF
+mv /tmp/Hack/*.ttf ~/.local/share/fonts/HackNF
+
+# Configure nvim
+rm -rf ~/.config/nvim
+mkdir -p ~/.config/nvim
+cp -rf /tmp/startupconfig/nvim ~/.config/nvim
+
+# Configure alacritty
+rm -rf ~/.config/alacritty
+mkdir -p ~/.config/alacritty
+cp -rf /tmp/startupconfig/alacritty ~/.config/alacritty
+
+# Install oh-my-zsh and plugins
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+
+# Set current user default terminal to zsh
+chsh $(loguser) -s $(which zsh)
+
+# Clean /tmp
+rm -rf /tmp/* 2> /dev/null
